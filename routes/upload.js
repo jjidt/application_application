f4033184470c28ba196c41ed361e904c2ea2da27
JJ Idt <johnidt@gmail.com>
2014-11-17 23:57:39 -0800

    add post to external api

--- routes/upload.js
+++ routes/upload.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var JSONrequest = require('request-json');
+var client = JSONrequest.newClient('http://localhost:8888/');
+var bufferString;
 
 router.post('/', function(req, res) {
 	var body = req.body;
-	var bufferString = req.files.resume.buffer.toString('base64');
-	body.projects = body.projects.split(",");
-	body.resume = bufferString;
-	res.send(body);
+	if (req.files && req.files.resume) {
+	 	bufferString = req.files.resume.buffer.toString('base64');
+	 	body.resume = bufferString;
+	} else {
+		res.send('no file uploaded');
+	}
+	if (body.projects) {
+		body.projects = body.projects.split(",")
+	}
+	client.post('', body, function(err, result, body) {
+		if(err) {
+			res.send(err);
+		}
+  			res.send(result.statusCode);
+		})
 });
 
 module.exports = router;
