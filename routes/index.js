f05b4ada3d6e10abce3d6320c0e8ba83de0cb632
JJ Idt <johnidt@gmail.com>
2014-11-18 00:53:57 -0800

    add generic title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Perka resume submission' });
+  res.render('index', { title: 'Resume Submission' });
 });
 
 router.post('/', function(req, res) {
